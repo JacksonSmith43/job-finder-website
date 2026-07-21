f21f9475546c751fc9732ba9dfc60c1ff88d009d
JacksonSmith43 <jessica444@gmx.at>
2026-07-21 09:51:22 +0200

    feat(navbar): Added Angular Material, Navbar actions and method stubs.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatButtonModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class Navbar {}
+export class Navbar {
+  onYourJobs() {
+    console.log('Navbar_onYourJobs().');
+  }
+
+  onResume() {
+    console.log('Navbar_onResume().');
+  }
+
+  onJobAlert() {
+    console.log('Navbar_onJobAlert().');
+  }
+
+  onSignUp() {
+    console.log('Navbar_onSignUp().');
+  }
+
+  onLogin() {
+    console.log('Navbar_onLogin().');
+  }
+
+  onRegister() {
+    console.log('Navbar_onRegister().');
+  }
+}
