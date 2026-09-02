167666d774b30c315202b164f8cc1c9db2579c7b
JacksonSmith43 <jessica444@gmx.at>
2026-09-02 11:18:32 +0200

    feat(application page): Scaffold application page component.

---
+++ src/app/application-page.component/application-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-application-page',
+  imports: [],
+  templateUrl: './application-page.component.html',
+  styleUrl: './application-page.component.css',
+})
+export class ApplicationPageComponent {}
