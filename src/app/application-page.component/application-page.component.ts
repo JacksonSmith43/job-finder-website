ad93f8cc5a7904d6a6fd084a8591fdde95b68b77
JacksonSmith43 <jessica444@gmx.at>
2026-09-03 12:27:35 +0200

    feat(application page): Basic structure and information.

--- src/app/application-page.component/application-page.component.ts
+++ src/app/application-page.component/application-page.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { DatePickerModule } from 'primeng/datepicker';
+
+import { LocalStorageService } from '../shared/services/local-storage.service';
+import { JobService } from '../shared/services/job.service';
+import { JobType } from '../shared/model/job-type.model';
 
 @Component({
   selector: 'app-application-page',
-  imports: [],
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    DatePickerModule,
+    MatDatepickerModule,
+    MatInputModule,
+  ],
   templateUrl: './application-page.component.html',
   styleUrl: './application-page.component.css',
 })
-export class ApplicationPageComponent {}
+export class ApplicationPageComponent implements OnInit {
+  localStorageService = inject(LocalStorageService);
+  jobService = inject(JobService);
+
+  currentJob = this.jobService.currentJob;
+
+  ngOnInit(): void {
+    console.log('ApplicationPageComponent_ngOnInit().');
+
+    let applySelectedJob = this.localStorageService.getFromLocalStorage('applyForSelectedJob');
+    this.currentJob.set(applySelectedJob as JobType);
+  }
+}
